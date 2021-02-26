c873be12bcda9d79f5def17e5034293f4c7fbc6b
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-02-25 22:29:14 -0600

    mvp v1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let display;
 let totalBooks = document.getElementById("total-books");
 let booksRead = document.getElementById("books-read");
 let pagesRead = parseInt(document.getElementById("pages-read").innerText, 10);
-// let totalPages = document.getElementById("total-pages");
+let totalPages = document.getElementById("total-pages");
 
 let myLibrary = [];
 
@@ -33,14 +33,12 @@ Book.prototype.haveRead = () => {
     }
 };
 
-
 closeIcon.onclick = () => {
     userForm.classList.add("hide")
 }
 
 addNewBook.addEventListener("click", () => {
     userForm.classList.remove("hide")
-    // container.classList.add("filter")
     bookTitle.focus()
     submit.onclick = () => validate();
     userForm.reset();
@@ -76,10 +74,13 @@ function addBookToLibrary() {
         display = div();
         display.id = "display"
 
+        let displaySide = div()
+        displaySide.id = "display-side"
+
         let title = div();
         title.innerText = book.title;
         title.id = "display-title"
-        display.appendChild(title);
+        displaySide.appendChild(title);
 
         let img = document.createElement("img");
         img.id = "display-img";
@@ -88,17 +89,17 @@ function addBookToLibrary() {
         let author = div();
         author.innerText = "by " + book.author;
         author.id = "display-author";
-        display.appendChild(author);
+        displaySide.appendChild(author);
 
         let pages = div();
-        pages.innerText = book.pages;
         pages.id = "display-pages";
-        display.appendChild(pages);
+        displaySide.appendChild(pages);
 
         let haveRead = div();
-        haveRead.innerText = book.haveRead();
         haveRead.id = "display-read";
-        display.appendChild(haveRead);
+        displaySide.appendChild(haveRead);
+
+        display.appendChild(displaySide)
 
         let googleApi = "https://www.googleapis.com/books/v1/volumes?q="
         fetch(googleApi + book.title + "+inauthor:" + book.author)
@@ -108,7 +109,8 @@ function addBookToLibrary() {
 
         function appendData(data) {
             img.src = data.items[0].volumeInfo.imageLinks.smallThumbnail;
-            // pageCounter = data.items[0].volumeInfo.pageCount;
+            pages.innerText = data.items[0].volumeInfo.pageCount + " pages";
+
         }
     })
 
@@ -123,106 +125,3 @@ function addBookToLibrary() {
 
 };
 
-
-
-
-
-
-
-
-
-    // totalPages += pageCounter;
-    // console.log(totalPages);
-
-
-// for (i = 0; i < myLibrary.length; i++) {
-//     let div = () => document.createElement("div");
-
-//     display = div();
-//     display.id = "display"
-
-//     let title = div();
-//     title.innerText = myLibrary[i].title;
-//     title.id = "display-title"
-//     display.appendChild(title);
-
-//     let img = document.createElement("img");
-//     img.id = "display-img";
-//     display.appendChild(img);
-
-//     let author = div();
-//     author.innerText = myLibrary[i].author;
-//     author.id = "display-author";
-//     display.appendChild(author);
-
-//     let pages = div();
-//     pages.innerText = myLibrary[i].pages;
-//     pages.id = "display-pages";
-//     display.appendChild(pages);
-
-//     let haveRead = div();
-//     haveRead.innerText = userBook.haveRead();
-//     haveRead.id = "display-read";
-//     display.appendChild(haveRead);
-
-
-//     let googleApi = "https://www.googleapis.com/books/v1/volumes?q="
-//     fetch(googleApi + myLibrary[i].title + "+inauthor:" + myLibrary[i].author)
-//         .then(response => response.json())
-//         .then(data => appendData(data))
-//         .catch(err => console.log(err));
-
-//     function appendData(data) {
-//         img.src = data.items[0].volumeInfo.imageLinks.smallThumbnail;
-//         userBook.pageCounter = data.items[0].volumeInfo.pageCount;
-//     }
-
-
-//     // totalPages += myLibrary[i].pageCounter;
-//     // console.log(myLibrary[i].pageCounter);
-// }
-
-
-// pageRead.map((page) => { page.checked ? console.log("true") : console.log('False') });
-        // display.innerText = myLibrary[i].title;
-
-        // for (let page of pageRead) {
-        //     if (page.checked) {
-        //         haveRead.innerText = false;
-        //     } else {
-        //         haveRead.innerText = true;
-        //     }
-        // }
-        // 
-// userBook.prototype = Object.create(Book.prototype);
-    // pageRead.map(e => {
-    //     if (e.checked) {
-    //         return
-    //     } else {
-    //         return e.value
-    //     }
-    // })
-    // myLibrary.map(e => {
-    //     display = document.createElement("div");
-    //     display.innerText = myLibrary[myLibrary.length - 1];
-    // })
-
-    // if (e.key === "Enter") {
-    // let book = new Book(bookTitle.value,)
-// let userInputValue = userInput.value;
-// userInput.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter") {
-//         let display = document.createElement("div");
-//         display.innerText = userInput.value;
-//         userInput.value ? container.appendChild(display) : console.log('empty');
-//         userInput.value = "";
-//     }
-// });
-
-
-
-//     let display = document.createElement("div");
-//     display.innerText = e;
-//     container.appendChild(display)
-//     console.log(myLibrary[myLibrary.length - 1]);
-// })
